c4e4995873536922d128496e491e2b03ca8b169a
Saira Asif <sasif01@uoguelph.ca>
2023-01-10 13:43:30 -0500

    Automate downloading files

---
+++ code/get_file.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#Make it DRY (do not repreat (yourself))
+
+#Specifiys that archive is the first ($1) command line argument.  
+archive = $1
+
+echo "$archive"
+
+#wget -nc -P dataa/raw/ https://rrndb.umms.med.umich.edu/static/download/rrndb-5.6_16S_rRNA.fasta.tsv.zip
